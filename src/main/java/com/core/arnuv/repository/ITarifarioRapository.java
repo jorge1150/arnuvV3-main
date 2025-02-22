92065861a79d70adc51244ee867368f1a54d55da
Jorge Mosquera <jorgemosquera1150@gmail.com>
2025-02-22 15:55:05 -0500

    jMS- se corrige tarifas

--- src/main/java/com/core/arnuv/repository/ITarifarioRapository.java
+++ src/main/java/com/core/arnuv/repository/ITarifarioRapository.java
@@ -1,6 +1,9 @@
 package com.core.arnuv.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.core.arnuv.model.Tarifario;
@@ -8,4 +11,6 @@ import com.core.arnuv.model.Tarifario;
 @Repository
 public interface ITarifarioRapository extends JpaRepository<Tarifario, Integer> {
 
+	@Query("Select r from Tarifario r where r.activo= ?1")
+	List<Tarifario> buscarPorEstado(Boolean estado);
 }
